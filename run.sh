08482c1934c7da12a1895cab07626b9adf4d7fc4
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2025-04-04 12:55:15 +0900

    update

---
+++ run.sh
@@ -0,0 +1,10 @@
+# 4/4
+# python -m slack_logger.slack_to_json --output-dir "./data" --last-days 30
+# python -m slack_logger.json_to_markdown --json-dir "./data/2025-03-04_to_2025-04-03" --weekly --all --output "./data/2025-03-04_to_2025-04-03/all_summary.md"
+
+# python -m github_logger.github_report --repo "digitaldemocracy2030/kouchou-ai" --markdown --last-days 30
+# python -m github_logger.github_report --repo "digitaldemocracy2030/idobata-analyst" --markdown --last-days 30
+# python -m github_logger.github_report --repo "digitaldemocracy2030/idobata-discourse-agent" --markdown --last-days 30
+# python -m github_logger.github_report --repo "digitaldemocracy2030/idobata-sns-agent" --markdown --last-days 30
+# python -m github_logger.github_report --repo "digitaldemocracy2030/idobata-infra" --markdown --last-days 30
+# python -m github_logger.github_report --repo "digitaldemocracy2030/website" --markdown --last-days 30
